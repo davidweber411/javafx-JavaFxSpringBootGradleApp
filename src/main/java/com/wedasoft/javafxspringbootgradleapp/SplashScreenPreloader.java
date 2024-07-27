6a1cd277504861141ec6b74c757ac5a76a1dd460
David Weber <weberdavid91@gmail.com>
2024-07-27 13:04:16 +0200

    edited splashscreen

--- src/main/java/com/wedasoft/javafxspringbootgradleapp/SplashScreenPreloader.java
+++ src/main/java/com/wedasoft/javafxspringbootgradleapp/SplashScreenPreloader.java
@@ -1,9 +1,11 @@
 package com.wedasoft.javafxspringbootgradleapp;
 
 import javafx.application.Preloader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -16,8 +18,19 @@ public class SplashScreenPreloader extends Preloader {
         stage = primaryStage;
         stage.initStyle(StageStyle.UNDECORATED);
 
-        Label splashLabel = new Label("Loading...");
-        StackPane root = new StackPane(splashLabel);
+        VBox vbox = new VBox(new Label("""
+                Loading...
+                        
+                Want to support open source?
+                        
+                Think about giving a star on Github!
+                        
+                https://github.com/davidweber411/javafx-JavaFxSpringBootGradleApp
+                        
+                https://github.com/davidweber411/javafx-JavaFxSpringBootMavenApp"""));
+        vbox.setSpacing(16);
+        vbox.setAlignment(Pos.CENTER);
+        StackPane root = new StackPane(vbox);
         Scene scene = new Scene(root, 400, 300);
         stage.setScene(scene);
         stage.show();
